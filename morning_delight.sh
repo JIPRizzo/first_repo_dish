cedc593b0568b777551c5965a62cf25f7522a2dc
jipø <jiprizzo@gmail.com>
2025-06-22 11:17:38 +0200

    Add morning shell script

---
+++ morning_delight.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#Greet the chef
+echo "Bonjour, José! ☀️ Hope you slept well."
+
+#Tell the date
+echo "Today is:"
+date
+
+#Open your favorite website (I can change this!)
+open https://dk.pinterest.com/josethereject/creative-compass-for-jrlp/
+
+#Sign off with good vibes
+echo "Bon appétit, maestro. 🎶💻🍞"
+
+
